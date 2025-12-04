b7e9bb937173ab95960e5fef79e5e7b464676405
EunsooSoh <no1_0218@naver.com>
2025-12-04 13:56:25 +0900

    [BE] login 기능 구현 및 user 별도의 API로 정리

--- BE/app/crud.py
+++ BE/app/crud.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from datetime import datetime
 
+
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
@@ -9,11 +10,11 @@ def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
+    # 해싱 없이 그대로 저장 (요청사항)
     db_user = models.User(
         email=user.email, 
-        hashed_password=fake_hashed_password,
-        investment_style=user.investment_style
+        hashed_password=user.password,
+        # investment_style은 나중에 설정
     )
     db.add(db_user)
     db.commit()
--- BE/app/schemas.py
+++ BE/app/schemas.py
@@ -5,7 +5,6 @@ class UserBase(BaseModel):
 
 class UserCreate(UserBase):
     password: str
-    investment_style: str | None = None
 
 class UserInvestmentUpdate(BaseModel):
     investment_style: str
